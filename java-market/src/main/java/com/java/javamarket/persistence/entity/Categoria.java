00f58514a427e3f22eecd9fc18d4c777a1cfa340
Luis Anderson <luisbruno777@gmail.com>
2020-12-22 15:37:19 -0500

    Arreglo entitys

--- java-market/src/main/java/com/java/javamarket/persistence/entity/Categoria.java
+++ java-market/src/main/java/com/java/javamarket/persistence/entity/Categoria.java
@@ -1,2 +1,39 @@
-package com.java.javamarket.persistence.entity;public class Categoria {
+package com.java.javamarket.persistence.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_categoria")
+    private Integer idCategoria;
+    private String descripcion;
+    private Boolean estado;
+
+    public Integer getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(Integer idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
 }
--- java-market/src/main/java/com/java/javamarket/persistence/entity/Cliente.java
+++ java-market/src/main/java/com/java/javamarket/persistence/entity/Cliente.java
@@ -1,2 +1,75 @@
-package com.java.javamarket.persistence.entity;public class Cliente {
+package com.java.javamarket.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    private String id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private String celular;
+
+    private String dirrecion;
+
+    @Column(name = "correo_electronico")
+    private String correoElectronico;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getDirrecion() {
+        return dirrecion;
+    }
+
+    public void setDirrecion(String dirrecion) {
+        this.dirrecion = dirrecion;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
 }
