9b252042e00ac2995c8be8b0a46a2516ba8b0efc
Luis Anderson <luisbruno777@gmail.com>
2020-12-22 16:16:27 -0500

    COLUMNAS EMBEBIDAS - Tabla muchos a muchos

--- java-market/src/main/java/com/java/javamarket/persistence/entity/ComprasProductoPK.java
+++ java-market/src/main/java/com/java/javamarket/persistence/entity/ComprasProductoPK.java
@@ -1,2 +1,30 @@
-package com.java.javamarket.persistence.entity;public class ComprasProductoPK {
+package com.java.javamarket.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class ComprasProductoPK {
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_producto")
+    private Integer idProducto;
+
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(Integer idProducto) {
+        this.idProducto = idProducto;
+    }
 }
