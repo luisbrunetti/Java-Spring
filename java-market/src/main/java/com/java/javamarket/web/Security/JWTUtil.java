41b8fc7de241429c5f08f668571dfbd479e8d46e
Luis Bruno <luisbruno777@gmail.com>
2020-12-25 21:31:22 -0500

    Autorización de la aplicación

--- java-market/src/main/java/com/java/javamarket/web/Security/JWTUtil.java
+++ java-market/src/main/java/com/java/javamarket/web/Security/JWTUtil.java
@@ -1,5 +1,6 @@
 package com.java.javamarket.web.Security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,5 +18,25 @@ public class JWTUtil {
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() * 1000* 60 *60 *10))
                 .signWith(SignatureAlgorithm.HS256,KEY).compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        //1.- Creado para el usuario
+        //2.- no haya experiado
+
+        //Primero se valida que el usuario que viene en la peteción en el mismo que se muestra en el tooken
+        return userDetails.getUsername().
+                equals(extractUsername(token))
+                && !isTokenExperired(token);
+    }
+    public String extractUsername(String token){
+        return getClaims(token).getSubject();
+    }
+    public boolean isTokenExperired(String token){
+        return getClaims(token).getExpiration().before(new Date());
+    }
+    //Claims == Clase en JWT
+    private Claims getClaims (String token){
+        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody();
     }
 }
--- java-market/src/main/java/com/java/javamarket/web/Security/SecurityCOnfig.java
+++ java-market/src/main/java/com/java/javamarket/web/Security/SecurityCOnfig.java
@@ -1,6 +1,7 @@
 package com.java.javamarket.web.Security;
 
 import com.java.javamarket.domain.service.UserDetailsService;
+import com.java.javamarket.web.Security.Filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,12 +11,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityCOnfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
@@ -31,11 +36,17 @@ public class SecurityCOnfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**/authenticate")
                 .permitAll()
                 .anyRequest()
-                .authenticated();
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //Sesión que seu utilizara sera STALESS = SIN SESION
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
-    @Bean
+    @Bean // Se esta escogiendo a el como gestor de authentificación
     public AuthenticationManager authenticationManagerBean() throws Exception {
         //Spring controlara la gestion de authentificación
         return super.authenticationManagerBean();
